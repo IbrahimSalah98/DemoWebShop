84602535de6235c26f21a1d802e5b1a9427017c8
zeinabmabd <zeinabmabd@gmail.com>
2024-09-26 21:00:00 +0300

    checkoutpage1

---
+++ src/test/java/com/tricentis/demowebshop/pages/CheckoutPage1.java
@@ -0,0 +1,67 @@
+package com.tricentis.demowebshop.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class CheckoutPage1 {
+
+
+    @FindBy(css = "[id=\"BillingNewAddress_CountryId\"]")
+    WebElement selectCountry;
+    @FindBy(css = "[id=\"BillingNewAddress_City\"]")
+    WebElement cityInput;
+    @FindBy(css = "[id=\"BillingNewAddress_Address1\"]")
+    WebElement address1Input;
+    @FindBy(css = "[id=\"BillingNewAddress_ZipPostalCode\"]")
+    WebElement zipCodeInput;
+    @FindBy(css = "[id=\"BillingNewAddress_PhoneNumber\"]")
+    WebElement phoneNumberInput;
+    @FindBy(css = "[class=\"button-1 new-address-next-step-button\"]")
+    WebElement clickContinue;
+
+
+    public CheckoutPage1(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+    
+    public void selectCountryByIndex(int index) {
+        Select selectCountryDropDown = new Select(selectCountry);
+        selectCountryDropDown.selectByIndex(index);
+    }
+
+    public void enterCity(String city) {
+        cityInput.sendKeys(city);
+    }
+
+    public void enterAddress1(String address) {
+        address1Input.sendKeys(address);
+    }
+
+    public void enterZipCode(String zipCode) {
+        zipCodeInput.sendKeys(zipCode);
+    }
+
+    public void enterPhoneNumber(String phoneNumber) {
+        phoneNumberInput.sendKeys(phoneNumber);
+    }
+
+    public void clickContinue() {
+        clickContinue.click();
+    }
+
+    public void fillAddressDetails() {
+        selectCountryByIndex(63);
+        enterCity("Qena");
+        enterAddress1("Qena Street");
+        enterZipCode("12345");
+        enterPhoneNumber("0123456789");
+        clickContinue();
+    }
+
+
+
+}
---
+++ src/test/java/com/tricentis/demowebshop/testcases/CheckOutTest.java
@@ -0,0 +1,36 @@
+package com.tricentis.demowebshop.testcases;
+
+import com.tricentis.demowebshop.base.BaseTest;
+import com.tricentis.demowebshop.pages.*;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CheckOutTest extends BaseTest {
+@Test
+    public void CheckOutPage1() throws InterruptedException {
+
+
+        driver.get("https://demowebshop.tricentis.com/login");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginFunction();
+        HomePage homePage = new HomePage(driver);
+        homePage.searchInput();
+        homePage.searchBtn();
+        driver.findElement(By.partialLinkText("Lap")).click();
+        ProductA productA = new ProductA(driver);
+        productA.changeQuantity("3");
+        productA.addToCart();
+        productA.shoppingCart();
+        CartPage cartPage = new CartPage(driver);
+        cartPage.termOfService();
+        cartPage.checkOut();
+        CheckoutPage1 checkoutPage1= new CheckoutPage1(driver);
+        checkoutPage1.fillAddressDetails();
+        Thread.sleep(10000);
+
+
+    }
+
+
+
+}
