881038fbb0361fa87bea502c69e4f782c0f92134
manarnasser1234 <nmanar79@gmail.com>
2024-10-07 04:47:11 +0300

    update

--- src/test/java/com/tricentis/demowebshop/pages/RegisterPage.java
+++ src/test/java/com/tricentis/demowebshop/pages/RegisterPage.java
@@ -1,6 +1,7 @@
 package com.tricentis.demowebshop.pages;
 
 import com.github.javafaker.Faker;
+import io.opentelemetry.sdk.metrics.internal.view.StringPredicates;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,16 +16,35 @@ public class RegisterPage {
     private WebElement gendermale;
     @FindBy(css="[id=\"gender-female\"]")
     private WebElement genderfemale;
+
     @FindBy(css = "[id=\"FirstName\"]")
     private WebElement firstName;
+    @FindBy(css = "[for=\"FirstName\"]")
+    private WebElement firstNameErr;
+
+
     @FindBy(css="[id=\"LastName\"]")
     private WebElement lastName;
+    @FindBy(css = "[for=\"LastName\"]")
+    private WebElement lastNameErr;
+
+
     @FindBy(css="[id=\"Email\"]")
     private WebElement email;
+    @FindBy(css = "[for=\"Email\"]")
+    private WebElement EmailErr;
+
     @FindBy(css="[id=\"Password\"]")
     private WebElement password;
+    @FindBy(css = "[for=\"Password\"]")
+    private WebElement passwordErr;
+
+
    @FindBy(css="[id=\"ConfirmPassword\"]")
    private WebElement confirmPassword;
+    @FindBy(css = "[ for=\"ConfirmPassword\"]")
+    private WebElement ConfirmpasswordErr;
+
    @FindBy(css="[id=\"register-button\"]")
    private WebElement register;
    @FindBy(css="[class=\"button-1 register-continue-button\"]")
@@ -41,6 +61,7 @@ public class RegisterPage {
     public void firstName(String FirstName){
         firstName.sendKeys(FirstName);
     }
+
     public void lastName(String LastName){
         lastName.sendKeys(LastName);
     }
@@ -61,10 +82,11 @@ public class RegisterPage {
         register_continueBU.click();
     }
 
-        String firstname=new Faker().name().firstName();
+    String firstname=new Faker().name().firstName();
     String lastname=new Faker().name().lastName();
     String Email=new Faker().internet().emailAddress();
     String Password=new Faker().internet().password();
+
     public void RegisterFN(){
         gendermale();
         firstName(firstname);
@@ -75,12 +97,43 @@ public class RegisterPage {
         register();
 
     }
-//    public String Email(){
-//        return Email;
-//    }
-//    public String password(){
-//        return Email;
-//    }
+    public void RegisterPage(String firstname, String lastname, String email, String password){
+        firstName(firstname);
+        lastName(lastname);
+        email(email);
+        password(password);
+        confirmPassword(password);
+    }
+
+    public void RegisterPage1(String firstname, String lastname, String email, String password,String ConfirmPassword){
+        firstName(firstname);
+        lastName(lastname);
+        email(email);
+        password(password);
+        confirmPassword(ConfirmPassword);
+    }
+
+    public String getFirstNameErrorMessage(){
+        return firstNameErr.getText();
+    }
+    public String getlastNameErrorMessage (){
+        return lastNameErr.getText();
+    }
+    public String getEmailErrorMessage (){
+        return EmailErr.getText();
+    }
+    public String getPasswordErrorMessage (){
+        return passwordErr.getText();
+    }
+    public String getConfirmPasswordErrorMessage (){
+        return ConfirmpasswordErr.getText();
+    }
+    public String register1(){
+       return register.getText();
+    }
+
+
+
 
 
 
--- src/test/java/com/tricentis/demowebshop/testcases/SignUpAsUserTest.java
+++ src/test/java/com/tricentis/demowebshop/testcases/SignUpAsUserTest.java
@@ -1,11 +1,13 @@
 package com.tricentis.demowebshop.testcases;
 
+import com.github.javafaker.Faker;
 import com.tricentis.demowebshop.base.BaseTest;
 import com.tricentis.demowebshop.pages.RegisterPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignUpAsUserTest extends BaseTest {
-   @Test
+   @Test(description = "Verify Main Functionality",priority = 1)
    public void shouldBeAbleToRegister() throws InterruptedException {
 
        RegisterPage registerPage= new RegisterPage(driver);
@@ -13,4 +15,84 @@ public class SignUpAsUserTest extends BaseTest {
        registerPage.register_continueBU();
        Thread.sleep(1000);
    }
+
+   @Test(description = "Verify_that_usr_canNot_SignUp_WithEmptyFirstName",priority = 2)
+    public void Verify_that_usr_canNot_SignUp_WithEmptyFirstName() throws InterruptedException {
+       RegisterPage registerPage=new RegisterPage(driver);
+       registerPage.gendermale();
+       String lastname=new Faker().name().lastName();
+       String Email=new Faker().internet().emailAddress();
+       String Password=new Faker().internet().password();
+       registerPage.RegisterPage("",lastname,Email,Password);
+       registerPage.register();
+       Thread.sleep(1000);
+       Assert.assertEquals(registerPage.getFirstNameErrorMessage(),"First name:");
+   }
+
+    @Test(description = "Verify_that_usr_canNot_SignUp_WithEmptylastName",priority = 3)
+    public void Verify_that_usr_canNot_SignUp_WithEmptylastName() throws InterruptedException {
+        RegisterPage registerPage=new RegisterPage(driver);
+        registerPage.gendermale();
+        String firstname=new Faker().name().firstName();
+        String Email=new Faker().internet().emailAddress();
+        String Password=new Faker().internet().password();
+        registerPage.RegisterPage(firstname,"",Email,Password);
+        registerPage.register();
+        Thread.sleep(1000);
+        Assert.assertEquals(registerPage.getlastNameErrorMessage(),"Last name:");
+    }
+
+    @Test(description = "Verify_that_usr_canNot_SignUp_WithEmptyEmail",priority = 4)
+    public void Verify_that_usr_canNot_SignUp_WithEmptyEmail() throws InterruptedException {
+        RegisterPage registerPage=new RegisterPage(driver);
+        registerPage.gendermale();
+        String firstname=new Faker().name().lastName();
+        String lastname=new Faker().name().lastName();
+        String Password=new Faker().internet().password();
+        registerPage.RegisterPage(firstname,lastname,"",Password);
+        registerPage.register();
+        Thread.sleep(1000);
+        Assert.assertEquals(registerPage.getEmailErrorMessage(),"Email:");
+    }
+
+    @Test(description = "Verify_that_usr_canNot_SignUp_WithpasswordLessthan_sixchar",priority = 5)
+    public void Verify_that_usr_canNot_SignUp_WithpasswordLessthan_sixchar() throws InterruptedException{
+        RegisterPage registerPage=new RegisterPage(driver);
+        registerPage.gendermale();
+        String firstname=new Faker().name().firstName();
+        String lastname=new Faker().name().lastName();
+        String Email=new Faker().internet().emailAddress();
+        registerPage.RegisterPage(firstname,lastname,Email,"12345");
+        registerPage.register();
+        Thread.sleep(1000);
+        Assert.assertEquals(registerPage.getPasswordErrorMessage(),"Password:");
+
+    }
+
+    @Test(description = "Verify_that_usr_canNot_SignUp_WithThepassword_and_confirmationpassword_donot_match",priority = 6)
+    public void Verify_that_usr_canNot_SignUp_WithThepassword_and_confirmationpassword_donot_match() throws InterruptedException{
+        RegisterPage registerPage=new RegisterPage(driver);
+        registerPage.gendermale();
+        String firstname=new Faker().name().firstName();
+        String lastname=new Faker().name().lastName();
+        String Email=new Faker().internet().emailAddress();
+        String Password=new Faker().internet().password();
+        String ConfirmPassword=new Faker().internet().password();
+        registerPage.RegisterPage1(firstname,lastname,Email,Password,ConfirmPassword);
+        registerPage.register();
+        Thread.sleep(1000);
+        Assert.assertEquals(registerPage.getPasswordErrorMessage(),"Password:");
+
+    }
+
+    @Test(description = "Verify_that_usr_canNot_SignUp_EmptyData",priority = 7)
+    public void Verify_that_usr_canNot_SignUp_EmptyData() throws InterruptedException{
+        RegisterPage registerPage=new RegisterPage(driver);
+        registerPage.register();
+        Thread.sleep(1000);
+        Assert.assertEquals(registerPage.register1(),"");
+
+    }
+
+
 }
